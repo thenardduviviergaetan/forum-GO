769306885b335950b8f8f705186026890ce0b9b4
qdelooze <a3nktr0@LAPTOP-S996C3C3>
2023-10-17 18:25:19 +0200

    remove unused function

--- internal/db/sqlite.go
+++ internal/db/sqlite.go
@@ -2,10 +2,9 @@ package forum
 
 import (
 	"database/sql"
-	"errors"
 	. "forum/pkg/models"
 
-	"github.com/mattn/go-sqlite3"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type App_db struct {
@@ -53,40 +52,18 @@ func (app *App_db) Migrate() error {
 	var count int
 	errChecker := app.DB.QueryRow("SELECT COUNT(*) FROM main_table").Scan(&count)
 	if errChecker == sql.ErrNoRows || count == 0 {
-		_, err = app.DB.Exec("INSERT INTO userstype(rank, label) VALUES (?,?)",1,"user")
+		_, err = app.DB.Exec("INSERT INTO userstype(rank, label) VALUES (?,?)", 1, "user")
 		if err != nil {
 			return err
 		}
-		_, err = app.DB.Exec("INSERT INTO userstype(rank, label) VALUES (?,?)",2,"moderator")
+		_, err = app.DB.Exec("INSERT INTO userstype(rank, label) VALUES (?,?)", 2, "moderator")
 		if err != nil {
 			return err
 		}
-		_, err = app.DB.Exec("INSERT INTO userstype(rank, label) VALUES (?,?)",3,"admin")
+		_, err = app.DB.Exec("INSERT INTO userstype(rank, label) VALUES (?,?)", 3, "admin")
 		if err != nil {
 			return err
 		}
 	}
 	return err
-}
-
-func (app *App_db) Create(post Post) (*Post, error) {
-	res, err := app.DB.Exec("INSERT INTO post(author, category, title, content, like, dislike) VALUES (?,?,?,?,?,?)")
-	if err != nil {
-		var sqliteErr sqlite3.Error
-		if errors.As(err, &sqliteErr) {
-			if errors.Is(sqliteErr.ExtendedCode, sqlite3.ErrConstraintUnique) {
-				return nil, err
-			}
-		}
-		return nil, err
-	}
-
-	id, err := res.LastInsertId()
-	if err != nil {
-		return nil, err
-	}
-
-	post.ID = id
-
-	return &post, nil
 }
