ff5ca62439846b7c2fce9660f9f8343900dc8a5b
gthenard <thenardduviviergaetan@gmail.com>
2023-10-18 12:47:19 +0200

    remove col like,dislike for tab post, creation table linkpost + add func updatelikepost

--- pkg/middleware/posts.go
+++ pkg/middleware/posts.go
@@ -3,6 +3,7 @@ package forum
 import (
 	"database/sql"
 	"fmt"
+
 	models "forum/pkg/models"
 )
 
@@ -19,5 +20,40 @@ func CreatePost(db *sql.DB, post *models.Post) (int, error) {
 }
 
 func UpdatePost(db *sql.DB, id int, post models.Post) {
+}
 
+func Updatelikepost(db *sql.DB, idpost, iduser int64, like bool) {
+	var exist bool
+	err := db.QueryRow("SELECT EXISTS( SELECT * FROM linkpost WHERE userid = ? AND postid = ?) AS exist", iduser, idpost).Scan(&exist)
+	if err != nil {
+		fmt.Println("Update like post: ", err)
+		return
+	}
+	if exist {
+		var current bool
+		err := db.QueryRow("SELECT likes FROM linkpost WHERE userid = ? AND postid = ?", iduser, idpost).Scan(&current)
+		if err != nil {
+			fmt.Println("Update like post: ", err)
+			return
+		}
+		if current == like {
+			_, err := db.Exec("DELETE FROM linkpost WHERE userid = ? AND postid = ?", iduser, idpost)
+			if err != nil {
+				fmt.Println("Update like post: ", err)
+				return
+			}
+		} else {
+			_, err := db.Exec("UPDATE linkpost SET likes = ? WHERE userid = ? AND postid = ?", like, iduser, idpost)
+			if err != nil {
+				fmt.Println("Update like post: ", err)
+				return
+			}
+		}
+	} else {
+		_, err := db.Exec("INSERT INTO linkpost(userid,postid,likes) VALUES(?,?,?)", iduser, idpost, like)
+		if err != nil {
+			fmt.Println("Update like post: ", err)
+			return
+		}
+	}
 }
