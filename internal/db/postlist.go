d03baeb3e9b9a1b754da14a62f86eec346a6e23b
qdelooze <a3nktr0@LAPTOP-S996C3C3>
2023-10-19 10:26:52 +0200

    add liked filter

--- internal/db/postlist.go
+++ internal/db/postlist.go
@@ -3,6 +3,7 @@ package forum
 import (
 	"html/template"
 	"net/http"
+
 	// "fmt"
 
 	models "forum/pkg/models"
@@ -23,65 +24,67 @@ func (app *App_db) PostHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if r.URL.Query().Has("created") {
-		app.ApplyFilter(w, r)
+	if r.URL.Query().Has("created") || r.URL.Query().Has("liked") {
+		if r.URL.Query().Get("created") == "true" {
+			app.CreatedFilter(w, r)
+		}
+		if r.URL.Query().Get("liked") == "true" {
+			app.LikedFilter(w, r)
+		}
+
 	} else {
 		rows, err := app.DB.Query("SELECT * FROM post")
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
-		
+
 		for rows.Next() {
 			err := rows.Scan(
-			&post.ID,
-			&post.AuthorID,
-			&post.Author,
-			&post.Categoryid,
-			&post.Title,
-			&post.Content,
-			&post.CreationDate,
-			&post.Flaged,
-		)
-		if err != nil {
+				&post.ID,
+				&post.AuthorID,
+				&post.Author,
+				&post.Categoryid,
+				&post.Title,
+				&post.Content,
+				&post.CreationDate,
+				&post.Flaged,
+			)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			err = app.DB.QueryRow("SELECT title FROM categories WHERE id=?", post.Categoryid).Scan(&post.Category)
+			post.User_like, post.User_dislike = linkpost(app, post.ID)
+			post.Like, post.Dislike = len(post.User_like), len(post.User_dislike)
+			app.Data.Posts = append(app.Data.Posts, post)
+		}
+
+		if err := rows.Err(); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-		
-		err = app.DB.QueryRow("SELECT title FROM categories WHERE id=?", post.Categoryid ).Scan(&post.Category)
-		post.User_like, post.User_dislike = linkpost(app, post.ID)
-		post.Like, post.Dislike = len(post.User_like), len(post.User_dislike)
-		app.Data.Posts = append(app.Data.Posts, post)
 	}
-	
-	if err := rows.Err(); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-}
-	
+
 	if err := tmpl.Execute(w, app.Data); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 }
 
-// TODO check for categories and liked filter 
-func (app *App_db) ApplyFilter(w http.ResponseWriter, r *http.Request) {
+// TODO check for categories and liked filter
+func (app *App_db) CreatedFilter(w http.ResponseWriter, r *http.Request) {
 	var post models.Post
 
-	f := r.URL.Query().Get("created")
-	
 	c, _ := r.Cookie("session_token")
-	authid := c.Value
 
-	if f == "true" {
-		rows, err := app.DB.Query("SELECT * FROM post WHERE authorid = ? ;", s.GlobalSessions[authid].UserID)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-		
-		for rows.Next() {
-			err := rows.Scan(
+	rows, err := app.DB.Query("SELECT * FROM post WHERE authorid = ? ;", s.GlobalSessions[c.Value].UserID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	for rows.Next() {
+		err := rows.Scan(
 			&post.ID,
 			&post.AuthorID,
 			&post.Author,
@@ -95,11 +98,57 @@ func (app *App_db) ApplyFilter(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-		
-		err = app.DB.QueryRow("SELECT title FROM categories WHERE id=?", post.Categoryid ).Scan(&post.Category)
+
+		err = app.DB.QueryRow("SELECT title FROM categories WHERE id=?", post.Categoryid).Scan(&post.Category)
 		post.User_like, post.User_dislike = linkpost(app, post.ID)
 		post.Like, post.Dislike = len(post.User_like), len(post.User_dislike)
 		app.Data.Posts = append(app.Data.Posts, post)
 	}
+}
+
+// TODO check for categories and liked filter
+func (app *App_db) LikedFilter(w http.ResponseWriter, r *http.Request) {
+	var post models.Post
+
+	c, _ := r.Cookie("session_token")
+
+	var tmp int
+	rows, err := app.DB.Query("SELECT postid FROM linkpost WHERE userid = ? ;", s.GlobalSessions[c.Value].UserID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	for rows.Next() {
+		err := rows.Scan(&tmp)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		// fmt.Println(tmp)
+		rows, err := app.DB.Query("SELECT * FROM post WHERE id = ? ;", tmp)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		for rows.Next() {
+			err := rows.Scan(
+				&post.ID,
+				&post.AuthorID,
+				&post.Author,
+				&post.Categoryid,
+				&post.Title,
+				&post.Content,
+				&post.CreationDate,
+				&post.Flaged,
+			)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			err = app.DB.QueryRow("SELECT title FROM categories WHERE id=?", post.Categoryid).Scan(&post.Category)
+			post.User_like, post.User_dislike = linkpost(app, post.ID)
+			post.Like, post.Dislike = len(post.User_like), len(post.User_dislike)
+			app.Data.Posts = append(app.Data.Posts, post)
+		}
 	}
 }
