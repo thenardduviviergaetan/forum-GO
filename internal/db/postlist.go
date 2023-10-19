fe4c8061eac5e20371772024499b2458f9c7b35d
qdelooze <a3nktr0@LAPTOP-S996C3C3>
2023-10-19 10:47:45 +0200

    add categories filter

--- internal/db/postlist.go
+++ internal/db/postlist.go
@@ -6,6 +6,7 @@ import (
 
 	// "fmt"
 
+	middle "forum/pkg/middleware"
 	models "forum/pkg/models"
 	s "forum/sessions"
 )
@@ -23,13 +24,18 @@ func (app *App_db) PostHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
-	if r.URL.Query().Has("created") || r.URL.Query().Has("liked") {
+	app.Data.Categories = middle.FetchCat(app.DB)
+	if r.URL.Query().Has("created") ||
+		r.URL.Query().Has("liked") ||
+		r.URL.Query().Has("categories") {
 		if r.URL.Query().Get("created") == "true" {
 			app.CreatedFilter(w, r)
 		}
 		if r.URL.Query().Get("liked") == "true" {
 			app.LikedFilter(w, r)
+		}
+		if r.URL.Query().Get("categories") != "" {
+			app.CatFilter(w, r)
 		}
 
 	} else {
@@ -106,7 +112,6 @@ func (app *App_db) CreatedFilter(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// TODO check for categories and liked filter
 func (app *App_db) LikedFilter(w http.ResponseWriter, r *http.Request) {
 	var post models.Post
 
@@ -150,5 +155,37 @@ func (app *App_db) LikedFilter(w http.ResponseWriter, r *http.Request) {
 			post.Like, post.Dislike = len(post.User_like), len(post.User_dislike)
 			app.Data.Posts = append(app.Data.Posts, post)
 		}
+	}
+}
+
+func (app *App_db) CatFilter(w http.ResponseWriter, r *http.Request) {
+	var post models.Post
+	cat_id := r.URL.Query().Get("categories")
+
+	rows, err := app.DB.Query("SELECT * FROM post WHERE categoryid = ? ;", cat_id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	for rows.Next() {
+		err := rows.Scan(
+			&post.ID,
+			&post.AuthorID,
+			&post.Author,
+			&post.Categoryid,
+			&post.Title,
+			&post.Content,
+			&post.CreationDate,
+			&post.Flaged,
+		)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		err = app.DB.QueryRow("SELECT title FROM categories WHERE id=?", post.Categoryid).Scan(&post.Category)
+		post.User_like, post.User_dislike = linkpost(app, post.ID)
+		post.Like, post.Dislike = len(post.User_like), len(post.User_dislike)
+		app.Data.Posts = append(app.Data.Posts, post)
 	}
 }
