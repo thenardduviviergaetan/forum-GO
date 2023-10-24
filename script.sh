2b82f7a6706aa301ddfe9085d21b35d583b94fdb
Robin <valentinrobin1@gmail.com>
2023-10-24 13:58:46 +0200

    added required to post

--- script.sh
+++ script.sh
@@ -1,7 +1,9 @@
 docker image build -f Dockerfile -t forum .
 
-docker container run -p 8080:8080 --detach --name forum forum
+# docker container run -p 8080:8080 --detach --name forum forum
 
-powershell.exe /c start http://localhost:8080
+docker container run -p 8080:8080 --name forum forum
 
-# firefox --new-window --full-sreen http://localhost:8080
+# powershell.exe /c start http://localhost:8080
+
+firefox --new-window --full-sreen http://localhost:8080
