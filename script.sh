2deb1f87811acf7f713555012b3d8736f6d27685
qdelooze <a3nktr0@LAPTOP-S996C3C3>
2023-10-29 22:44:09 +0100

    valid script for team

--- script.sh
+++ script.sh
@@ -4,6 +4,6 @@ docker image build -f Dockerfile -t forum .
 
 docker container run -p 8080:8080 --name forum forum
 
-powershell.exe /c start https://localhost:443
+# powershell.exe /c start https://localhost:8080
 
 firefox --new-window --full-sreen http://localhost:8080
