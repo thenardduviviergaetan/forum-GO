2eba61eea1f6823100e602190e561393d321eb75
qdelooze <a3nktr0@LAPTOP-S996C3C3>
2023-10-29 23:20:19 +0100

    improve autocert gen / add goroutine/channel + mutex(in comments) mfor token bucket algorithm

--- config/tls.go
+++ config/tls.go
@@ -2,24 +2,43 @@ package forum
 
 import (
 	"log"
+	"os"
 	"os/exec"
 )
 
 // Auto generate keys and self-signed certificates for ssl/tls connections
 func GenerateCert() {
+	var missed bool
+	files := []string{"ca-key.pem", "ca.pem", "cert-key.pem", "cert.pem", "cert.csr", "ca.srl"}
 
-	commands := []string{
-		"openssl genrsa -aes256 -out ca-key.pem 4096",
-		"openssl req -new -x509 -sha256 -days 365 -key ca-key.pem -out ca.pem -config openssl_forum.cnf",
-		"openssl genrsa -out cert-key.pem 4096",
-		"openssl req -new -sha256 -subj \"/CN=forum_group\" -key cert-key.pem -out cert.csr",
-		"openssl x509 -req -sha256 -days 365 -in cert.csr -CA ca.pem -CAkey ca-key.pem -out cert.pem -extfile openssl_forum.cnf -CAcreateserial",
-	}
-
-	for _, cmd := range commands {
-		err := exec.Command("bash", "-c", cmd).Run()
-		if err != nil {
-			log.Fatalf("Command execution failed: %s", err)
+	for _, file := range files {
+		if !FileExist(file) {
+			missed = true
 		}
 	}
+
+	if missed {
+		commands := []string{
+			"openssl genrsa -aes256 -out ca-key.pem 4096",
+			"openssl req -new -x509 -sha256 -days 365 -key ca-key.pem -out ca.pem -config openssl_forum.cnf",
+			"openssl genrsa -out cert-key.pem 4096",
+			"openssl req -new -sha256 -subj \"/CN=forum_group\" -key cert-key.pem -out cert.csr",
+			"openssl x509 -req -sha256 -days 365 -in cert.csr -CA ca.pem -CAkey ca-key.pem -out cert.pem -extfile openssl_forum.cnf -CAcreateserial",
+		}
+
+		for _, cmd := range commands {
+			err := exec.Command("bash", "-c", cmd).Run()
+			if err != nil {
+				log.Fatalf("Command execution failed: %s", err)
+			}
+		}
+	}
+}
+
+func FileExist(file string) bool {
+	info, err := os.Stat(file)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
 }
--- sessions/tokenBucket.go
+++ sessions/tokenBucket.go
@@ -2,57 +2,110 @@ package forum
 
 import (
 	"net/http"
-	"sync"
 	"time"
 )
 
 type TokenBucket struct {
-	tokens              int
+	tokens              chan struct{}
 	maxTokens           int
 	refillRate          time.Duration
 	lastRefillTimestamp time.Time
-	mutex               sync.Mutex
 }
 
 // Initialize a new token bucket
 func NewBucket(maxTokens int, refillRate time.Duration) *TokenBucket {
-	return &TokenBucket{
-		tokens:              maxTokens,
-		maxTokens:           maxTokens,
-		refillRate:          refillRate,
-		lastRefillTimestamp: time.Now(),
+	tb := &TokenBucket{
+		tokens:     make(chan struct{}, maxTokens),
+		maxTokens:  maxTokens,
+		refillRate: refillRate,
+	}
+
+	for i := 0; i < maxTokens; i++ {
+		tb.tokens <- struct{}{}
+	}
+	go tb.refill()
+	return tb
+}
+
+// Refill bucket
+func (tb *TokenBucket) refill() {
+	ticker := time.NewTicker(tb.refillRate)
+	for range ticker.C {
+		select {
+		case tb.tokens <- struct{}{}:
+		default:
+		}
 	}
 }
 
 // Check if request is Allowed. If true, sub token to bucket
-func (tb *TokenBucket) Allow() bool {
-	tb.mutex.Lock()
-	defer tb.mutex.Unlock()
-
-	elapsed := time.Now().Sub(tb.lastRefillTimestamp)
-	tokenToAdd := int(elapsed / tb.refillRate)
-
-	if tokenToAdd > 0 {
-		if tb.tokens+tokenToAdd < tb.maxTokens {
-			tb.tokens = tb.tokens + tokenToAdd
-		} else {
-			tb.tokens = tb.maxTokens
-		}
-	}
-	if tb.tokens > 0 {
-		tb.tokens--
+func (tb *TokenBucket) allow() bool {
+	select {
+	case <-tb.tokens:
 		return true
+	default:
+		return false
 	}
-	return false
 }
 
 // Custom handler with token bucket limiter for rate limiting
 func HandleWithLimiter(path string, handler func(http.ResponseWriter, *http.Request), limiter *TokenBucket) {
 	http.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
-		if !limiter.Allow() {
+		if !limiter.allow() {
 			http.Error(w, "Too many requests", http.StatusTooManyRequests)
 			return
 		}
 		handler(w, r)
 	})
 }
+
+// type TokenBucket struct {
+// 	tokens              int
+// 	maxTokens           int
+// 	refillRate          time.Duration
+// 	lastRefillTimestamp time.Time
+// 	mutex               sync.Mutex
+// }
+
+// // Initialize a new token bucket
+// func NewBucket(maxTokens int, refillRate time.Duration) *TokenBucket {
+// 	return &TokenBucket{
+// 		tokens:              maxTokens,
+// 		maxTokens:           maxTokens,
+// 		refillRate:          refillRate,
+// 		lastRefillTimestamp: time.Now(),
+// 	}
+// }
+
+// // Check if request is Allowed. If true, sub token to bucket
+// func (tb *TokenBucket) Allow() bool {
+// 	tb.mutex.Lock()
+// 	defer tb.mutex.Unlock()
+
+// 	elapsed := time.Now().Sub(tb.lastRefillTimestamp)
+// 	tokenToAdd := int(elapsed / tb.refillRate)
+
+// 	if tokenToAdd > 0 {
+// 		if tb.tokens+tokenToAdd < tb.maxTokens {
+// 			tb.tokens = tb.tokens + tokenToAdd
+// 		} else {
+// 			tb.tokens = tb.maxTokens
+// 		}
+// 	}
+// 	if tb.tokens > 0 {
+// 		tb.tokens--
+// 		return true
+// 	}
+// 	return false
+// }
+
+// // Custom handler with token bucket limiter for rate limiting
+// func HandleWithLimiter(path string, handler func(http.ResponseWriter, *http.Request), limiter *TokenBucket) {
+// 	http.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+// 		if !limiter.Allow() {
+// 			http.Error(w, "Too many requests", http.StatusTooManyRequests)
+// 			return
+// 		}
+// 		handler(w, r)
+// 	})
+// }
